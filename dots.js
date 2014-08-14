c54ba6e9cc7d52cc591b4ff08847b564fb52612d
Andrew Taylor <andrew.taylor@webappuk.com>
2014-08-14 13:20:21 +0100

    dancing basically works

--- dots.js
+++ dots.js
@@ -7,7 +7,7 @@ var options = {
 	generalised: [3, 4, 5, 6],
 	totalDelay: 500, // ms
 	showNumbers: false,
-	margin: 10,
+	margin: 0.5,
 	sumDistance: 1.2
 }, update;
 
@@ -22,20 +22,20 @@ console.log(Object.keys(options));
 
 (function(){
 	angular.module('dots', [])
-	.directive('dotpanel', ['$timeout', function(timeout) {
+	.directive('dotPanel', ['$timeout', function(timeout) {
 		var panel,
 			hidingTransitionTime = 300; //ms, must match css
 
 		// remove all dots with an index of n or above
 		function hideDotsFrom(n) {
 			var d = 0, dStep = options.totalDelay / options.maxN;
-			for (; n < maxN; ++n) {
-				var el = document.getElementById('dot-' + n);
+			for (; n < options.maxN; ++n) {
+				var el = angular.element(document.getElementById('dot-' + n));
 				if (el) {
 					timeout(function() {
-						el.classList.add('hiding');
+						el.addClass('hiding');
 						timeout(function() {
-							panel.removeChild(el);
+							el.remove();
 						}, hidingTransitionTime);
 					}, d);
 					d += dStep;
@@ -52,25 +52,29 @@ console.log(Object.keys(options));
 				el.classList.add('dot');
 				el.classList.add('hiding');
 				el.id = id;
-				panel.appendChild(el);
+				panel.append(angular.element(el));
 			}	
 			el.style.top = x + 'px';
-			el.style.bottom = y + 'px';
+			el.style.left = y + 'px';
 			el.style.width = el.style.height = size + 'px';
 			return timeout(function() {
 				el.classList.remove('hiding');
 			});
 		}
 
+		function delayPut(i, x, y, size, delay) {
+			timeout(function() {
+				putDot(i, x, y, size);
+			}, delay);
+		}
+
 		// perform a dance
 		function dance(d) {
 			hideDotsFrom(d.length);
-			var d = 0, dStep = options.totalDelay / options.maxN;
+			var delay = 0, dStep = options.totalDelay / options.maxN;
 			for (var i = 0; i < d.length; ++i) {
-				timeout(function() {
-					putDot(i, d[i].x, d[i].y, d.size);
-				}, d);
-				d += dStep;
+				delayPut(i, d[i].x, d[i].y, d.size, delay);
+				delay += dStep;
 			}
 		}
 
@@ -90,23 +94,30 @@ console.log(Object.keys(options));
 
 		// make sure a dance fits in the panel
 		function squeezeDance(d) {
-			var minx = Infinity, maxx = -Infinity, miny = Infinity, maxy = -Infinity;
+			var minx = Infinity, 
+				maxx = -Infinity,
+				miny = Infinity,
+				maxy = -Infinity,
+				margin = d.size + options.margin;
 			for (var i = 0; i < d.length; ++i) {
 				if (d[i].x < minx) minx = d[i].x;
 				if (d[i].x > maxx) maxx = d[i].x;
 				if (d[i].y < miny) miny = d[i].y;
 				if (d[i].y > maxy) maxy = d[i].y;
 			};
-			maxx += d.size + margin; minx -= d.size + margin;
-			maxy += d.size + margin; miny -= d.size + margin;
-			var h = panel.clientHeight, w = panel.clientWidth,
-				dh = maxy - miny, dw - maxx - minx;
-				m = Math.min(h / dh, w / dw),
-				cx = ((minx + maxx) * m + w) * .5,
-				cy = ((miny + maxy) * m + h) * .5;
+			maxx += margin;
+			minx -= margin;
+			maxy += margin;
+			miny -= margin;
+			var h = panel.prop('clientHeight'),
+				w = panel.prop('clientWidth'),
+				dh = maxy - miny,
+				dw = maxx - minx,
+				m = Math.min(h / dh, w / dw);
+				// TODO - this top-lefts it and it should centre
 			for (var i = 0; i < d.length; ++i) {
-				d[i].x = m * d[i].x + cx;
-				d[i].y = m * d[i].y + cy;
+				d[i].x = m * d[i].x - minx;
+				d[i].y = m * d[i].y - miny;
 			}
 			d.size = d.size * m;
 			return d;
@@ -114,11 +125,21 @@ console.log(Object.keys(options));
 
 		return {
 			scope: {
-				n: '='
+				dance: '='
 			},
 			link: function(scope, element, attrs) {
 				panel = element;
-				update = scope.$apply();
+				update = scope.$apply;
+				scope.squeezeDance = squeezeDance;
+				scope.sumDances = sumDances;
+				var nullDance = [];
+				nullDance.size = 0;
+				scope.$watch('dance', function(d) {
+					if (d)
+						dance(d);
+					else
+						dance(nullDance);
+				});
 			}
 		};
 	}]);
