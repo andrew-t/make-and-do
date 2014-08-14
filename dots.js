c60128c3d9e042b78775169a4db2a38e89662179
Andrew Taylor <shiny@andrewt.net>
2014-08-14 08:06:40 +0100

    Dots

--- dots.js
+++ dots.js
@@ -119,6 +119,9 @@ console.log(Object.keys(options));
 			link: function(scope, element, attrs) {
 				panel = element;
 				update = scope.$apply();
+				scope.dance = dance;
+				scope.sumDances = sumDances;
+				scope.squeezeDance = squeezeDance;
 			}
 		};
 	}]);
