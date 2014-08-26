5af6eb79d372632eb328b5d1968af53e61be8651
Andrew Taylor <mac@andrewt.net>
2014-08-26 19:05:41 +0100

    nicer build

--- _build.js
+++ _build.js
@@ -4,15 +4,34 @@ var mangle = true,
 	liftVars = true,
 	outDir = 'dist',
 	output = 'gadgets.min.js',
-	dependencies = ['angular.min.js', 'big.js/big.min.js'];
+	dependencies = ['angular.min.js', 'big.js/big.min.js'],
+	noConcat = [/worker\.js$/i, /^prime\.js$/];
 
 var fs = require('fs'),
 	uglify = require('./UglifyJS/uglify-js.js'),
-	files = fs.readdirSync('.');
+	files = fs.readdirSync('.'),
+	totalIn = 0,
+	totalOut = 0;
+
+try {
+	fs.mkdirSync(outDir);
+} catch(e) { }
+
+function forEachFile(extension, callback) {
+	var rightType = new RegExp('\.' + extension + '$', 'i');
+	files.forEach(function(filename) {
+		if (/^_/.test(filename) || !rightType.test(filename))
+			return;
+		var file = fs.statSync(filename);
+		if (file.isDirectory())
+			return;
+		callback(filename);
+	});
+}
 
 // JavaScript
 
-function minify(code) {
+function minify(code, fn) {
 	var ast = uglify.parser.parse(code);
 
 	if (liftVars)
@@ -27,31 +46,45 @@ function minify(code) {
 
 	var minified = uglify.uglify.gen_code(ast);
 
+	totalIn += code.length;
+	totalOut += minified.length;
+	fs.writeFileSync(outDir + '/' + fn, minified);
+
+	console.log('File: ' + fn);
 	console.log('Original length: ' + code.length);
 	console.log('Minified length: ' + minified.length);
 	console.log('Compression ratio: ' + (minified.length / code.length));
+	console.log('');
 
 	return minified;
 }
 
-files.forEach(function(filename) {
-	if (/^_/.test(filename) || /\.min\.js$/i.test(filename) || !/\.js$/i.test(filename))
-		return;
-	var file = fs.statSync(filename);
-	if (file.isDirectory())
-		return;
-	fs.writeFileSync(outDir + '/' + filename, 
-		minify(fs.readFileSync(filename, 'utf8').replace(/['"]use strict['"];/gi, '')));
+var code = '';
+
+forEachFile('js', function(filename) {
+	if (!/\.min\.js$/i.test(filename)) {
+		var content = fs.readFileSync(filename, 'utf8').replace(/['"]use strict['"];/gi, '') + '\n\n',
+			concat = true;
+		noConcat.forEach(function(filter) {
+			if (filter.test(filename))
+				concat = false;
+		});
+		if (concat)
+			code += content;
+		else
+			minify(content, filename);
+	}
 });
+
+minify(code, output);
+
+console.log('Total original length: ' + totalIn);
+console.log('Total minified length: ' + totalOut);
+console.log('Compression ratio: ' + (totalOut / totalIn));
 
 // HTML
 
-files.forEach(function(filename) {
-	if (/^_/.test(filename) || !/\.html$/i.test(filename))
-		return;
-	var file = fs.statSync(filename);
-	if (file.isDirectory())
-		return;
+forEachFile('html', function(filename) {
 	fs.writeFileSync(outDir + '/' + filename, fs.readFileSync(filename, 'utf8')
 		.replace(/<script src=".*\/([^\/]+).min.js"><\/script>/ig, 
 				'<script src="$1.min.js"></script>'));
@@ -59,12 +92,7 @@ files.forEach(function(filename) {
 
 // CSS
 
-files.forEach(function(filename) {
-	if (/^_/.test(filename) || !/\.css$/i.test(filename))
-		return;
-	var file = fs.statSync(filename);
-	if (file.isDirectory())
-		return;
+forEachFile('css', function(filename) {
 	fs.writeFileSync(outDir + '/' + filename, 
 		fs.readFileSync(filename, 'utf8')
 		.replace(/\/\*.*?\*\//gm, '')
